0addfcfe7ae2c47bc22b8008f1b56a52c9f8bc06
Robbile <robbilekondimane@outlook.fr>
2020-06-30 11:58:28 +0100

    microservices discussions
    CRUD operations OK!!

--- tcool/src/main/java/com/kf2y/tcool/repository/DiscussionRepository.java
+++ tcool/src/main/java/com/kf2y/tcool/repository/DiscussionRepository.java
@@ -1,5 +1,7 @@
 package com.kf2y.tcool.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kf2y.tcool.domain.Discussion;
@@ -7,5 +9,7 @@ import com.kf2y.tcool.domain.Discussion;
 
 
 public interface DiscussionRepository extends JpaRepository<Discussion, Long>{
+	
+	public List<Discussion> findByStatus(String status);
 
 }
---
+++ tcool/src/main/java/com/kf2y/tcool/service/DiscussionService.java
@@ -0,0 +1,26 @@
+package com.kf2y.tcool.service;
+
+import java.util.List;
+
+import com.kf2y.tcool.domain.Discussion;
+
+public interface DiscussionService {
+
+	public Discussion save( Discussion discussion);
+	
+	public void delete(Long id);
+	
+	public Discussion getById(Long id);
+	
+	public List<Discussion> getAllDiscussions();
+	
+	// liste des discussions en cours
+	public List<Discussion> getDiscussionEnCours();
+	
+	// liste des discussions clôturées
+	public List<Discussion> getDiscussionCloturees();
+	
+	// liste des discussions rejetées
+	public List<Discussion> getDiscussionRejetees();
+	
+}
