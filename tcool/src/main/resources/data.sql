558f425fbd6a31c2819e0028aabf7fbd02137eef
mehdibenyoussef50 <mehdibenyoussef50@gmail.com>
2020-07-04 19:17:13 +0100

    message

--- tcool/src/main/resources/data.sql
+++ tcool/src/main/resources/data.sql
@@ -13,26 +13,21 @@ INSERT INTO `compte_my_role` (`compte_id_compte`, `my_role_id`) VALUES
 (2, 1)
 ;
 
-INSERT INTO `Discussion` (`created_by`, `created_date`, `last_modified_date`, `details`,`status`, `title`, `visibility`) VALUES
-('Robb', now(), now(), 'Discussion 1', 'EN COURS', 'Discussion 1', 'Public'),
-('Robb', now(), now(), 'Discussion 2', 'EN COURS', 'Discussion 2', 'Public'),
-('Robb', now(), now(), 'Discussion 3', 'EN COURS', 'Discussion 3', 'Public'),
-('Robb', now(), now(), 'Discussion 4', 'EN COURS', 'Discussion 4', 'Public'),
-('Robb', now(), now(), 'Discussion 5', 'CLOTUREE', 'Discussion 5', 'Public'),
-('Robb', now(), now(), 'Discussion 6', 'CLOTUREE', 'Discussion 6', 'Public'),
-('Robb', now(), now(), 'Discussion 7', 'REJETEE', 'Discussion 7', 'Public'),
-('Robb', now(), now(), 'Discussion 8', 'REJETEE', 'Discussion 8', 'Public'),
-('Robb', now(), now(), 'Discussion 9', 'REJETEE', 'Discussion 9', 'Public');
+INSERT INTO `Discussion` (`created_by`, `created_date`, `last_modified_date`, `details`,`status`, `title`, `version`, `visibility`) VALUES
+('Robb', now(), now(), 'Discussion 1', 'EN COURS', 'Discussion 1',0, 'Public'),
+('Robb', now(), now(), 'Discussion 2', 'EN COURS', 'Discussion 2',0, 'Public'),
+('Robb', now(), now(), 'Discussion 3', 'EN COURS', 'Discussion 3',0, 'Public'),
+('Robb', now(), now(), 'Discussion 4', 'EN COURS', 'Discussion 4',0, 'Public'),
+('Robb', now(), now(), 'Discussion 5', 'CLOTUREE', 'Discussion 5',0, 'Public'),
+('Robb', now(), now(), 'Discussion 6', 'CLOTUREE', 'Discussion 6',0, 'Public'),
+('Robb', now(), now(), 'Discussion 7', 'REJETEE', 'Discussion 7',0, 'Public'),
+('Robb', now(), now(), 'Discussion 8', 'REJETEE', 'Discussion 8',0, 'Public'),
+('Robb', now(), now(), 'Discussion 9', 'REJETEE', 'Discussion 9',0, 'Public');
 
-INSERT INTO `Intervention` (`created_by`, `created_date`, `last_modified_date`, `details`,`status`, `title`,`version`) VALUES
-('Mehdi', now(), now(), 'Intervention 1', 'EN COURS', 'Intervention 1',0),
-('Mehdi', now(), now(), 'Intervention 2', 'EN COURS', 'Intervention 2',0),
-('Mehdi', now(), now(), 'Intervention 3', 'EN COURS', 'Intervention 3',0),
-('Mehdi', now(), now(), 'Intervention 4', 'EN COURS', 'Intervention 4',0),
-('Mehdi', now(), now(), 'Intervention 5', 'CLOTUREE', 'Intervention 5',0),
-('Mehdi', now(), now(), 'Intervention 6', 'CLOTUREE', 'Intervention 6',0),
-('Mehdi', now(), now(), 'Intervention 7', 'ANNULEE', 'Intervention 7',0),
-('Mehdi', now(), now(), 'Intervention 8', 'ANNULEE', 'Intervention 8',0),
-('Mehdi', now(), now(), 'Intervention 9', 'ANNULEE', 'Intervention 9',0),
-('Mehdi', now(), now(), 'Intervention 10', 'EN COURS', 'Intervention 10',0);
+INSERT INTO `Intervention` (`created_by`, `created_date`, `last_modified_date`, `details`,`status`, `title`,`version`,`location`) VALUES
+('Mehdi', now(), now(), 'Intervention 1', 'EN COURS', 'Intervention 1',0,'13 Rue Al Adarissa, Rabat 10020, Morocco'),
+('Mehdi', now(), now(), 'Intervention 2', 'EN COURS', 'Intervention 2',0,'22 Avenue Michlifen, Rabat 10080, Morocco'),
+('Mehdi', now(), now(), 'Intervention 3', 'EN COURS', 'Intervention 3',0,'n10 Rue el Baoudi, Rabat 10000, Morocco'),
+('Mehdi', now(), now(), 'Intervention 4', 'CLOTUREE', 'Intervention 5',0,'20 Rue Ghana, Rabat, Morocco'),
+('Mehdi', now(), now(), 'Intervention 5', 'ANNULEE', 'Intervention 5',0,'Avenue Sidi Moussa, Salé, Morocco');
 
